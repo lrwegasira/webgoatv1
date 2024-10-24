3cdb07d7a7b649157881a3eb1a4614ab89ab8ab7
lrwegasira <rwegasiralucy@gmail.com>
2024-10-24 04:59:54 -0700

    My Initial Commit

---
+++ src/main/java/com/unique/webgoatv1/Webgoatv1Application.java
@@ -0,0 +1,13 @@
+package com.unique.webgoatv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webgoatv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webgoatv1Application.class, args);
+	}
+
+}
